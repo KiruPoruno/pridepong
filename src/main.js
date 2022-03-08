73bd25594ac43c62d529073d8f1fdac5d1c1e7d5
Kiru <mail@kiru.gay>
2022-03-09 00:21:17 +0100

    somewhat working ball

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ setInterval(() => {
 	if (down("s")) {move("left", 1)}
 	if (down("ArrowUp")) {move("right", -1)} else
 	if (down("ArrowDown")) {move("right", 1)}
+
+	moveBall()
 }, 1)
 
 
@@ -35,6 +37,31 @@ $.addEventListener("keyup", (e) => {
 	}
 })
 
+// 
+// Bounds
+//
+
+var bounds = {
+	top: () => {
+		return 0
+	},
+	left: () => {
+		return 0
+	},
+	right: (div) => {
+		let right = main.getBoundingClientRect().width;
+		right -= div.getBoundingClientRect().width;
+		right -= parseInt(getComputedStyle(main).getPropertyValue("--padding")) * 2;
+		return right
+	},
+	bottom: (div) => {
+		let bottom = main.getBoundingClientRect().height;
+		bottom -= div.getBoundingClientRect().height;
+		bottom -= parseInt(getComputedStyle(main).getPropertyValue("--padding")) * 2;
+		return bottom
+	}
+}
+
 //
 // Movement
 //
@@ -55,19 +82,71 @@ function move(paddle, cord) {
 		case "right":
 			div = $.querySelector(".player.right");
 			break;
+		case "ball":
+			div = ball;
+			break;
 		default:
 			throw "Unknown paddle"
 	}
 
-	let bottom = main.getBoundingClientRect().height;
-	bottom -= div.getBoundingClientRect().height;
-	bottom -= parseInt(getComputedStyle(main).getPropertyValue("--padding")) * 2;
-
 	newcord = paddles[paddle] + cord;
-	if (newcord < 0) {return}
-	if (newcord > bottom) {return}
+	if (newcord < bounds.top()) {return}
+	if (newcord > bounds.bottom(div)) {return}
 
 	paddles[paddle] += cord;
 	cord = paddles[paddle];
 	div.style.transform = `translateY(${paddles[paddle]}px)`
+}
+
+// 
+// Ball
+//
+
+let ballpos = {
+	y: 0,
+	x: 0
+}
+
+let velocity = {
+	y: 1,
+	x: 1
+}
+
+function moveBall() {
+	let newy = ballpos.y + velocity.y;
+	let newx = ballpos.x + velocity.x;
+	let increaseY = -0.1;
+	let increaseX = -0.1;
+
+	if (Math.sign(newy) == -1) {increaseY *= -1}
+	if (Math.sign(newx) == -1) {increaseX *= -1}
+
+	if (velocity.y >= 15 || velocity.y <= -15) {increaseY = 0}
+	if (velocity.x >= 15 || velocity.x <= -15) {increaseX = 0}
+
+	if (newy < bounds.top()) {
+		velocity.y = (velocity.y * -1) + increaseY;
+		return;
+	}
+
+	if (newy > bounds.bottom(ball)) {
+		velocity.y = (velocity.y * -1) + increaseY;
+		return;
+	}
+
+
+	if (newx < bounds.left()) {
+		velocity.x = (velocity.x * -1) + increaseX;
+		return;
+	}
+
+	if (newx > bounds.right(ball)) {
+		velocity.x = (velocity.x * -1) + increaseX;
+		return;
+	}
+
+	ballpos.y = newy;
+	ballpos.x = newx;
+
+	ball.style.transform = `translateY(${newy}px) translateX(${newx}px)`
 }
